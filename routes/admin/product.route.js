b31047fc6703994c72e1a2e046c58572b4305a79
machgiahao <120111913+machgiahao@users.noreply.github.com>
2024-08-31 11:26:15 +0700

    update upload cloud and fix env

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const multer  = require('multer');
-const storangeMulter = require("../../helpers/storangeMulter.js");
-const upload = multer({ storage: storangeMulter() });
+const multer = require('multer');
+// const storangeMulter = require("../../helpers/storangeMulter.js");
+// const upload = multer({ storage: storangeMulter() });
+
+const upload = multer();
+
 
 const route = express.Router()
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
 
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 route.get("/", controller.index);
 route.patch("/change-status/:status/:id", controller.changeStatus); // : dùng để truyền data động
@@ -15,16 +19,17 @@ route.patch("/change-multi", controller.changeMulti); // : dùng để truyền 
 route.delete("/delete/:id", controller.deleteItem); // : dùng để truyền data động
 route.get("/create", controller.create);
 route.post(
-    "/create", 
-    upload.single("thumbnail"), 
+    "/create",
+    upload.single("thumbnail"),
+    uploadCloud.upload,
     validate.createPost, // middleware: là trung gian check xem request có thỏa mãn ko thì mới được chạy tiếp
     controller.createPost
 );
 
 route.get("/edit/:id", controller.edit); // :id dùng để truyền data động
 route.patch(
-    "/edit/:id", 
-    upload.single("thumbnail"), 
+    "/edit/:id",
+    upload.single("thumbnail"),
     validate.createPost, // middleware: là trung gian check xem request có thỏa mãn ko thì mới được chạy tiếp
     controller.editPatch
 );
